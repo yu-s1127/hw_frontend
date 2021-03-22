d84e4f3ca51d23bce2d6c8104cc256c813f514cd
s-y1127 <yooopopo5@gmail.com>
2021-03-23 01:07:20 +0900

    crete canvas functions

--- src/components/HandWritten.jsx
+++ src/components/HandWritten.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 
 const HandWritten = () => {
+  const canvasRef = useRef(null);
+  const [drawing, setDrawing] = useState(false);
+
   const canvasWidth = 120;
   const canvasHeight = 120;
+
+  const startDrawing = () => {
+    setDrawing(true);
+
+    const canvas = canvasRef.current.getContext('2d');
+    canvas.beginPath();
+  };
+
+  const endDrawing = () => {
+    setDrawing(false);
+  };
+
+  const clearDrawing = () => {
+    const canvas = canvasRef.current.getContext('2d');
+    canvas.clearRect(0, 0, canvasWidth, canvasHeight);
+  };
+
+  const draw = (x, y) => {
+    if (!drawing) return;
+    const canvas = canvasRef.current.getContext('2d');
+    canvas.lineTo(x, y);
+    canvas.stroke();
+  };
 
   return (
     <SContainer>
       <h1>Hand Written Recognition</h1>
 
-      <SCanvas width={canvasWidth} height={canvasHeight} />
+      <SCanvas
+        width={canvasWidth}
+        height={canvasHeight}
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseLeave={endDrawing}
+        onMouseMove={(e) => draw(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
+      />
 
       <SButtonContainer>
-        <SButton>reset</SButton>
+        <SButton onClick={clearDrawing}>reset</SButton>
         <SButton>predict</SButton>
       </SButtonContainer>
     </SContainer>
@@ -25,6 +59,7 @@ const SCanvas = styled.canvas`
 const SButton = styled.button`
   width: 10vw;
   height: 5vh;
+  cursor: pointer;
 `;
 const SContainer = styled.div`
   display: grid;
