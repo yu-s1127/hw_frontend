48b916b19dcc4edcd0c13d971d1f8fca9653cf45
s-y1127 <yooopopo5@gmail.com>
2021-03-23 05:20:59 +0900

    create radio button

--- src/components/HandWritten.jsx
+++ src/components/HandWritten.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { Bar } from 'react-chartjs-2';
@@ -8,9 +8,13 @@ const HandWritten = () => {
   const [drawing, setDrawing] = useState(false);
   const [predictedLabel, setPredictedLabel] = useState('');
   const [predictionProb, setPredictionProb] = useState([]);
+  const [base64String, setBase64String] = useState('');
 
   const canvasWidth = 128;
   const canvasHeight = 128;
+
+  const modelList = ['simple', 'auged'];
+  const [selectedModel, setSelectedModel] = useState(modelList[0]);
 
   const data = {
     labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
@@ -62,25 +66,50 @@ const HandWritten = () => {
     const canvas = canvasRef.current;
 
     resizedContext.drawImage(canvas, 0, 0, 28, 28);
-    const base64String = resizedCanvas.toDataURL('image');
+    setBase64String(resizedCanvas.toDataURL('image'));
     // const base64String = canvasRef.current.toDataURL('image/png');
     // console.log(base64String);
+  };
 
-    const response = await axios
-      .post('http://localhost:8000/api/predict', {
-        image: base64String,
-      })
-      .catch((err) => {
-        console.log('an error occured while predicting');
-      });
+  useEffect(() => {
+    if (base64String) {
+      (async () => {
+        const response = await axios
+          .post('http://localhost:8000/api/predict', {
+            image: base64String,
+            model_type: selectedModel,
+          })
+          .catch((err) => {
+            console.log('an error occured while predicting');
+          });
+        setPredictedLabel(response.data.predicted_label);
+        setPredictionProb(response.data.prediction_prob);
+      })();
+    }
+  }, [base64String, selectedModel]);
 
-    setPredictedLabel(response.data.predicted_label);
-    setPredictionProb(response.data.prediction_prob);
+  const onSelectedModelChange = (e) => {
+    setSelectedModel(e.target.value);
   };
 
   return (
     <SContainer>
-      <h1>Hand Written Recognition</h1>
+      <h1>手書き文字予測</h1>
+
+      <SModelList>
+        {modelList.map((model, index) => (
+          <div key={index}>
+            <input
+              type="radio"
+              value={model}
+              name={model}
+              checked={selectedModel === model}
+              onChange={onSelectedModelChange}
+            />
+            <label>{model}</label>
+          </div>
+        ))}
+      </SModelList>
 
       <SCanvas
         width={canvasWidth}
@@ -98,7 +127,7 @@ const HandWritten = () => {
       </SButtonContainer>
 
       <div>
-        {predictedLabel === '' || <h2>Predicted: {predictedLabel}</h2>}
+        {predictedLabel === '' || <h2>予測値: {predictedLabel}</h2>}
         <Bar data={data} />
       </div>
     </SContainer>
@@ -123,6 +152,9 @@ const SButtonContainer = styled.div`
   button:first-child {
     margin-right: 0.5vh;
   }
+`;
+const SModelList = styled.div`
+  display: flex;
 `;
 
 export default HandWritten;
